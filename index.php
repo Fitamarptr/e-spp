5169d8a72e6681760276a8d0223560971bc0a684
Muhammad Fazry Suhada <91445555+fzry18@users.noreply.github.com>
2023-05-12 00:56:33 +0800

    Update index.php

--- index.php
+++ index.php
@@ -11,6 +11,8 @@
   <link rel="stylesheet" href="assets/fontawesome-free/css/all.css">
 
   <!-- Template CSS -->
+  <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="stylesheet" href="assets/css/components.css">
   <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="css/components.css">
 
